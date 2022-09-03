b269b44b53e600cf2d515fe2a791637c593940f9
Thanh Hieu <hieu.dang.dth_is_me@hcmut.edu.vn>
2022-09-03 17:00:42 +0700

    update gitignore

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 app.use(cors());
 app.use(express.json());
